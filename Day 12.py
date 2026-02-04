e29d4a72230af776e671ffefa0ba94e332280b14
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-02-04 19:52:19 +0500

    Add files via upload

---
+++ Day 12.py
@@ -0,0 +1,50 @@
+# Learning Python - Day 12
+# 1. What is While Loops In Python?
+# 2. Why While Loops Are Important?
+# 3. While Loop With Break Statement.
+# 4. While Loop With Continue Statement.
+
+# Practical Exercise Of The Day 12
+
+# Examples:
+
+# # Asks Until Valid Number
+# num = ""
+# while not num.isnumeric():
+#     num = input("Enter A Number:")
+#     print("Valid Number IF The Input Is In Numbers:", num)
+
+
+# # Loop Through List Using While
+# items = ["Laptop", "Mouse", "Keyboard"]
+# i = 0
+# while i < len(items):
+#     print(items[i])
+#     i += 1
+
+
+# # While Loop With Break Statement
+# x = 0
+# while x<=10:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
+
+
+# # While Loop With Continue Statement
+# x = 0
+# while x<=10:
+#     x += 1
+#     if x % 2 == 0:
+#         continue
+#     print(x)
+ 
+
+
+
+
+
+
+
+
