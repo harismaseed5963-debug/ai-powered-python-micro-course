95d2925cb514e4d57e5ed7dd07c3bf36bbdf6d28
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-02-04 19:51:56 +0500

    Add files via upload

---
+++ Day 11.py
@@ -0,0 +1,60 @@
+# Learning Python - Day 11
+# 1. What Is For Loop In Python?
+# 2. Why For Is Important for Data Analysts?
+
+# Practical Exercise Of The Day 11
+
+# Examples:
+
+# # Print 1 To 5:
+# for i in range(1,6):
+#     print(i)
+
+# # Print "Hello" 5 Times
+# for i in range(1,6):
+#     print("Hello")
+
+# # Looping Over A List
+# cars = ["Lambergini","Toyota", "Civic", "Suzuki"]
+# for i in cars:
+#     print(i)
+
+# # Looping Over A Sting
+# name = "Haris"
+# for i in name:
+#     print(i)
+
+# Loop With Range:
+
+# print("Print All Even  Between 0 & 20")
+# for i in range(0,21,2):
+#     print(i)
+
+# #  Total Marks Calculation
+# marks = [34,65,76]
+# total = 0
+# for m in marks:
+#     total += m
+#     print("Total Marks:",total)
+
+# # Cleaning Cities Names
+# cities = ["mUMbai","DELhi","pune","Chennai"]
+# cleaned = []
+# for c in cities:
+#     cleaned.append(c.title().strip())
+#     print(cleaned)
+
+# # Fixing Spelling Mistakes
+# wrong_list = ["Bengluru", "Mombai", "Kolkatta"]
+# correct_list = []
+# for city in wrong_list:
+#     correct_list.append(city.replace("Bengluru","Bengaluru")
+#                         .replace("Mombai","Mumbai")
+#                         .replace("Kolkatta","Kolkata"))
+# print(correct_list)
+
+
+
+
+
+
