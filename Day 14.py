548fad8dfd604185a4e7a28b520b5e28727c45e2
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-02-04 23:13:21 +0500

    Add files via upload

---
+++ Day 14.py
@@ -0,0 +1,46 @@
+# Learning Python - Day 14
+# 1. What Are Tupples In Python?
+# 2. Tuple Vs Lists
+# 3. Packing And Unpacking In Tuples
+# 4. Nested Tuples Inside Lists
+
+# Practical Exercise Of The Day 14
+
+# # Tuples Are Immutable
+# print("Tupples Are Immutable")
+# num = (1,2,3)
+# num(1) = 4
+# print(num) # Output : Error
+
+# # Loopinh Through Tuples
+colors = ("Blue", "Orange", "Yellow")
+# for c in colors:
+#     print(c)
+
+# # Tuple Length
+# print(len(colors))
+
+# # Concatenation Of Tuples
+# a = (1,2)
+# b = (3,4)
+# c = a + b
+# print(c) # Output : (1,2,3,4)
+
+# # Packing & Unpacking(Very Important)
+# data = ("Haris","03468700981","Tank")
+# name,number,city = data
+# print(name)
+# print(number)
+# print(city)
+
+# Nested Tuples Inside List
+
+# employees = [
+#  ("E101", "Haris", "Tank"),
+#  ("E102", "Hasher", "Waziristan"),
+#  ("E103", "Ayesha", "DIKhan")
+# ]
+# for e_id, name, city in employees:
+#  print(name, "from", city)
+
+
