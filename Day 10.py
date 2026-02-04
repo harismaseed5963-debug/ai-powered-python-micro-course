f476825fc4f6f51c3f3853e0a79be4dc33e97e27
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-02-04 19:51:32 +0500

    Add files via upload

---
+++ Day 10.py
@@ -0,0 +1,40 @@
+# Learning Python - Day 10
+# Assignments Day 
+
+# Practical Exercise Of Th Day 10
+
+# # Multi_Level Verification System Software
+
+# security_code = int(input("Enter Your Security Code:"))
+
+# if security_code==5566:
+#     department = input("Enter Your Department:")
+#     if department.title() == "Finance":
+#         access_level = int(input("Enter Your Access Level"))
+#         if access_level >=5:
+#             print("Access Granted Welcome To The Meeting Room")
+#         else:
+#             print("Insufficient Access Level")
+#     else:
+#      print("Access Denied : Department Not Allowed")
+# else:
+#      print("Invalid Security Code")
+
+
+# # Online Exam Login
+# reg_no = int(input("Enter Your Registration Number:"))
+# if reg_no == 1221:
+#     exam_sub = input("Dear Student Please Enter Your Exam Subject:")
+#     if exam_sub.lower() == "python":
+#         password = int(input("Dear Student Please Enter The Password:"))
+#         if password == 8888:
+#             print("Login Successfully : Start Your Exam")
+#         else:
+#             "Wrong Password"
+#     else:
+#         print("Subject Not Available")
+# else:
+#     print("Registration Failed")
+
+
+
