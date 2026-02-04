deced5f487891efa4c7674c5c0c6d04ced46aaf6
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-02-04 19:48:00 +0500

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,34 @@
+# # Learning Python - Day 3
+# # 1. Data Types In Python
+# # 2. Why Data types are Matters in Python?
+# # 3. Different Types Of Data In Python
+# # 4. Text Types, Numeric Types, Boolean Types, Sequence Types, Mapping Types, Set Types,None Types
+
+# # Practical Exercise Of The Day 3
+# # Text Types
+# first_name = "Haris"          # => String Data Type
+# last_name = 'Ur Rehman'      # => String Data Type
+
+# # Numeric Types
+# age = 18                      # => Integer Data Type
+# height = 5.9                  # => Float Data Type
+# complex_number = 3 + 4j     # => Complex Data Type
+
+# # Boolean Types
+# is_student = True             # => Boolean Data Type
+# has_passed = False           # => Boolean Data Type
+
+# # Sequence Types
+# fruits = ["Apple", "Banana", "Cherry"]  # => List Data Type
+# colors = ("Red", "Green", "Blue")        # => Tuple Data Type
+# letters = "ABCDE"                        # => String Data Type
+
+# # Mapping Types
+# person = {"name": "Haris", "age": 18}    # => Dictionary Data Type
+
+# # Set Types
+# unique_numbers = {1, 2, 3, 4, 5}  # => Set Data Type
+
+# # None Type
+# nothing = None                    # => None Data Type
+
