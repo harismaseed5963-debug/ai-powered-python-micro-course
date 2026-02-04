fe15522ad3be8e1d9df816877d2b47d9025e27dc
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-02-04 19:45:36 +0500

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,14 @@
+# # Learning Python - Day 1 
+# # 1.What Is Python?
+# # 2.Why Python For Data Analysts?
+# # 3.Downloading & Installing Python
+# # 4.VA Code Setup
+# # 5.Writing Your First Python
+ 
+# Practical Exercise Of The Day 1
+
+# # Print First Program In Python
+
+# print("Hello, World!")   # Output: Hello, World!
+# print("My name is Haris Ur Rehman")  # Output: My name is Haris Ur Rehman
+# print("I am learning Python for Data Analysis")  # Output: I am learning Python for Data Analysis
