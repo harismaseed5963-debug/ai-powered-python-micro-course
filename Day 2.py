ffb9a45ec565fd3c667f694b41b487709283f83f
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-02-04 19:46:55 +0500

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,29 @@
+# # Learning Python - Day 2
+# # 1. Comments & Variables In Python
+# # 2. What Are Comments & Why they are Important?
+# # 3. What are variables & rules to create variables in Python?
+
+# # Practical Exercise Of The Day 2
+# name = "Haris Ur Rehman"      # => Text Variable Data Stored In Quotes
+# age = 18                      # => Numeric Variable Data Without Quotes
+# sales = 40000                 # => Numeric Variable Data Without Quotes
+# print("My Name Is :" ,name)
+# print("I am",age, "Years Old")
+# print("My Today Sales Is", sales)
+
+
+# # Mini Assignment Of The Day
+# # Use 5 Prints,3 Comments, 5 Variables To Complete An Example Program
+# # Example Program:
+# # Variables
+# product_name = "Laptop"
+# product_price = 80000
+# product_quantity = 5
+# customer_name = "Ali"
+# customer_budget = 100000
+# # Print Statements
+# print("Product Name:", product_name)  # Output: Product Name: Laptop
+# print("Product Price:", product_price)  # Output: Product Price: 80000
+# print("Product Quantity:", product_quantity)  # Output: Product Quantity: 5
+# print("Customer Name:", customer_name)  # Output: Customer Name: Ali
+# print("Customer Budget:", customer_budget)  # Output: Customer Budget: 100000
