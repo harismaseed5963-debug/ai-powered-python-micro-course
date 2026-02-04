97c1ceb1f6edfb94b98466d3d062e1d9f4fd8d09
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-02-04 19:50:45 +0500

    Add files via upload

---
+++ Day 8.py
@@ -0,0 +1,39 @@
+# Learning Python  - Day 8
+# 1. What is Else_if Funtion In Python?
+# 2. Why its Matter?
+# 3. Basic Syntex
+# 4. if-elif-else Funtion In Python?
+
+# Practical Exercise Of The Day 8
+
+# # Use Cases Of else-if funtion 
+
+# # String Comparison
+# string = input("Enter Your Name:")
+# if string.title() == "Haris":
+#     print("Matched")
+# else:
+#     print("Not Matched")
+
+# # Email Validation
+# email = input("Enter Your Email:")
+# if "@" in email and "." in email:
+#     print("Your email is Verified")
+# else:
+#     print("Your Email Is Not Available")
+
+# # Missing Data Check
+# value = input("Enter a Value:")
+# if value == "":
+#     print("Missing Value")
+
+
+
+
+
+
+
+
+
+
+
